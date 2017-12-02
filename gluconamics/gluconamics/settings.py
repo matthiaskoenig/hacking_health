f0eaaa448a22893b6409d6618e453174cfa71aaa
Matthias Koenig <konigmatt@googlemail.com>
2017-12-02 03:04:14 +0100

    fixing webpages

--- gluconamics/gluconamics/settings.py
+++ gluconamics/gluconamics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^7r9z!xy82$tg$wnds8!68((_k*i&bm-2h+c_c%acol=l+h*j&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -134,4 +134,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# for djange debug toolbar
+INTERNAL_IPS = ['127.0.0.1', ]
+
+LOGIN_REDIRECT_URL = '/index'
+LOGIN_URL = '/login/'
